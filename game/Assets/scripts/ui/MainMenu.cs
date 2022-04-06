3cb7fdedf1077611ab453f3a83907ddabf3010c6
BlackMetalV <58908228+BlackMetalV@users.noreply.github.com>
2022-04-06 15:09:35 +0400

    Updated UI
    Fixed buttons and applied new style

--- game/Assets/scripts/ui/MainMenu.cs
+++ game/Assets/scripts/ui/MainMenu.cs
@@ -1,9 +1,12 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    public string TestLevel;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -14,5 +17,25 @@ public class MainMenu : MonoBehaviour
     void Update()
     {
         
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene(TestLevel);
+    }
+
+    public void OpenMainOptions()
+    { 
+    
+    }
+
+    public void CloseMainOptions()
+    { 
+    
+    }
+
+    public void CloseGame()
+    {
+        Application.Quit();
     }
 }
