cf3f8a11f5856750bda7e87c6de3824b0a87e58f
BlackMetalV <58908228+BlackMetalV@users.noreply.github.com>
2022-04-27 14:41:39 +0300

    path renderer and cam control

--- game/Assets/scripts/player/PlayerController.cs
+++ game/Assets/scripts/player/PlayerController.cs
@@ -4,14 +4,28 @@ using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.InputSystem;
 
+[RequireComponent(typeof(NavMeshAgent))]
+[RequireComponent(typeof(LineRenderer))]
 public class PlayerController : MonoBehaviour
 {
-    public 
+    private NavMeshAgent playerAgent;
+
+    private GameObject clickMarker;
+    private LineRenderer walkPath;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerAgent = GetComponent<NavMeshAgent>();
+        clickMarker = GameObject.Find("ClickMarker");
+        clickMarker.transform.localScale = new Vector3(0, 0, 0);
+        walkPath = GetComponent<LineRenderer>();
+
+        walkPath.startWidth = 0.05f;
+        walkPath.endWidth = 0.05f;
+        walkPath.positionCount = 0;
+
+        LeanTween.reset();
     }
 
     // Update is called once per frame
@@ -21,6 +35,17 @@ public class PlayerController : MonoBehaviour
         {
             MoveToClick();
         }
+
+        if (Vector3.Distance(playerAgent.destination, transform.position) <= playerAgent.stoppingDistance)
+        {
+            clickMarker.transform.localScale = new Vector3(0, 0, 0);
+        }
+        else if (playerAgent.hasPath)
+        {
+            DrawPath();
+        }
+
+        clickMarker.transform.Rotate(new Vector3(0, 0, -Time.deltaTime * 50));
     }
 
     private void MoveToClick()
@@ -31,7 +56,35 @@ public class PlayerController : MonoBehaviour
 
         if(isHit)
         {
-            GetComponent<NavMeshAgent>().destination = hit.point;
+            SetDestination(hit.point);            
+        }       
+    }
+
+    private void SetDestination(Vector3 target)
+    {
+        playerAgent.SetDestination(target);
+        clickMarker.transform.position = new Vector3(target.x, 0.2f, target.z);
+        clickMarker.transform.localScale = new Vector3(0, 0, 0);
+        LeanTween.scale(clickMarker, new Vector3(0.2f, 0.2f, 0.2f), 0.2f);
+    }
+
+    private void DrawPath()
+    {
+        walkPath.positionCount = playerAgent.path.corners.Length;
+        walkPath.SetPosition(0, transform.position);
+
+        if (playerAgent.path.corners.Length < 2)
+        {
+            return;
+        }
+
+
+        for (int i = 1; i < playerAgent.path.corners.Length; i++)
+        {
+            Vector3 pointPosition = new Vector3(playerAgent.path.corners[i].x, playerAgent.path.corners[i].y, 
+                playerAgent.path.corners[i].z);
+            walkPath.SetPosition(i, pointPosition);
+
         }
     }
 }
