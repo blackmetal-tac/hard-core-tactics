abe4fe95380fdbe17a6bdefc2ff5e6d863387769
BlackMetalV <58908228+BlackMetalV@users.noreply.github.com>
2022-07-08 13:39:43 +0300

    new UI

--- game/Assets/scripts/audio/SongName.cs
+++ game/Assets/scripts/audio/SongName.cs
@@ -4,42 +4,45 @@ using TMPro;
 public class SongName : MonoBehaviour
 {
     private AudioClip song;
-    private static TextMeshProUGUI textmeshPro;
-    
-    public static RectTransform maskPos, textPos;
-    public static Vector3 startPosition;
-    private static float width;
-    private float scrollPos;
+    private TextMeshProUGUI textmeshPro;
+    private float width, scrollPos, startPos, posX;
+    private RectTransform rectTransform;
 
     // Start is called before the first frame update
     void Start()
     {
-        song = GameObject.Find("AudioManager").GetComponent<AudioSource>().clip;
-        maskPos = GameObject.Find("Mask").GetComponent<RectTransform>();
-        textPos = this.gameObject.GetComponent<RectTransform>();
-        textmeshPro = GetComponent<TextMeshProUGUI>();    
+        song = GameObject.Find("AudioManager").GetComponent<AudioSource>().clip;      
+        textmeshPro = GetComponent<TextMeshProUGUI>();
         scrollPos = 0;
-
-        startPosition = maskPos.position;
+        //startPos = textmeshPro.transform.position.x;
+        startPos = transform.localPosition.x;
+        posX = startPos;
     }
 
     // Update is called once per frame
     void Update()
     {       
-        //Double string and spaces
-        textmeshPro.text = "\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0" + song.name.ToString() +           
-        "\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0" + song.name.ToString();
+        //Double strings and spaces
+        textmeshPro.text = "\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0" + song.name.ToString() +
+        "\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0\u00A0" + song.name.ToString();
 
         //Set reset modifier for animation
-        width = textmeshPro.preferredWidth / 2;
+        width = textmeshPro.preferredWidth / 2;        
 
         //Text animation
-        textmeshPro.transform.position = new Vector3(-scrollPos + startPosition.x, startPosition.y, startPosition.z);
-        scrollPos += 1 * 1 * Time.deltaTime / 50;
+        //textmeshPro.transform.position = new Vector3(-scrollPos + posX,
+          //  textmeshPro.transform.position.y, textmeshPro.transform.position.z);
 
-        if (textPos.localPosition.x <= -width)
+        transform.localPosition = new Vector3(-scrollPos + posX,
+            transform.localPosition.y, transform.localPosition.z);
+        scrollPos += Time.deltaTime / 2000;
+
+        Debug.Log(transform.localPosition.x);
+
+        if (transform.localPosition.x <= -width)
         {
             scrollPos = 0;
+            posX = startPos;
         }
     }
 }
