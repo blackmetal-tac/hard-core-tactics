894688efec02d4e6e6b9fb15aecbb692fdb3850c
BlackMetalV <58908228+BlackMetalV@users.noreply.github.com>
2022-07-28 13:48:29 +0300

    fix weapon ui lists!!!

--- game/Assets/scripts/ui/WeaponUI.cs
+++ game/Assets/scripts/ui/WeaponUI.cs
@@ -6,38 +6,52 @@ using TMPro;
 public class WeaponUI : MonoBehaviour
 {
     private WPNManager rightWPN;
-    private Slider rightWPNui;
-    private TextMeshProUGUI weaponName, modeText;
     private GameManager gameManager;
-    private List<ActionMask> weaponMasks;
+
+    private class WeaponButton
+    {
+        public TextMeshProUGUI weaponName, mode;
+        public Slider slider;
+        public ActionMask actionMask;
+
+        public WeaponButton(TextMeshProUGUI newWeapon, Slider newSlider, TextMeshProUGUI newMode, ActionMask newMask)
+        {
+            weaponName = newWeapon;
+            slider = newSlider;
+            mode = newMode;
+            actionMask = newMask;            
+        }
+    }
+
+    private List<WeaponButton> weaponButtons;
 
     // Start is called before the first frame update
     void Start()
     {      
-        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();
-        rightWPNui = GameObject.Find("RightArmUI").GetComponentInChildren<Slider>();
+        gameManager = GameObject.Find("GameManager").GetComponent<GameManager>();        
 
         /* Fill the list of all weapon slots (ORDER: rigth arm, left arm, rigth top,
               left top, rigth shoulder, left shoulder) */
-        weaponMasks = new List<ActionMask>();
-        weaponMasks.Add(GameObject.Find("RightArmUI").transform.parent.GetComponentInChildren<ActionMask>());
-        weaponMasks.Add(GameObject.Find("LeftArmUI").transform.parent.GetComponentInChildren<ActionMask>());
-        weaponMasks.Add(GameObject.Find("RightArmUI").transform.parent.GetComponentInChildren<ActionMask>());
-        weaponMasks.Add(GameObject.Find("RightArmUI").transform.parent.GetComponentInChildren<ActionMask>());
-        weaponMasks.Add(GameObject.Find("RightArmUI").transform.parent.GetComponentInChildren<ActionMask>());
-        weaponMasks.Add(GameObject.Find("RightArmUI").transform.parent.GetComponentInChildren<ActionMask>());
+        weaponButtons = new List<WeaponButton>();
+        weaponButtons.Add(new WeaponButton(GameObject.Find("RightArmUI").transform.Find("Weapon").GetComponent<TextMeshProUGUI>(), GameObject.Find("RightArmUI").GetComponentInChildren<Slider>(), GameObject.Find("RightArmUI").transform.Find("Slider").Find("Handle Slide Area").Find("Handle").GetComponentInChildren<TextMeshProUGUI>(), GameObject.Find("RightArmUI").transform.parent.GetComponentInChildren<ActionMask>()));
+        weaponButtons.Add(new WeaponButton(GameObject.Find("LeftArmUI").transform.Find("Weapon").GetComponent<TextMeshProUGUI>(), GameObject.Find("LeftArmUI").GetComponentInChildren<Slider>(), GameObject.Find("LeftArmUI").transform.Find("Slider").Find("Handle Slide Area").Find("Handle").GetComponentInChildren<TextMeshProUGUI>(), GameObject.Find("LeftArmUI").transform.parent.GetComponentInChildren<ActionMask>()));
+        weaponButtons.Add(new WeaponButton(GameObject.Find("RightTopUI").transform.Find("Weapon").GetComponent<TextMeshProUGUI>(), GameObject.Find("RightTopUI").GetComponentInChildren<Slider>(), GameObject.Find("RightTopUI").transform.Find("Slider").Find("Handle Slide Area").Find("Handle").GetComponentInChildren<TextMeshProUGUI>(), GameObject.Find("RightTopUI").transform.parent.GetComponentInChildren<ActionMask>()));
+        weaponButtons.Add(new WeaponButton(GameObject.Find("LeftTopUI").transform.Find("Weapon").GetComponent<TextMeshProUGUI>(), GameObject.Find("LeftTopUI").GetComponentInChildren<Slider>(), GameObject.Find("LeftTopUI").transform.Find("Slider").Find("Handle Slide Area").Find("Handle").GetComponentInChildren<TextMeshProUGUI>(), GameObject.Find("LeftTopUI").transform.parent.GetComponentInChildren<ActionMask>()));
+        weaponButtons.Add(new WeaponButton(GameObject.Find("RightShoulderUI").transform.Find("Weapon").GetComponent<TextMeshProUGUI>(), GameObject.Find("RightShoulderUI").GetComponentInChildren<Slider>(), GameObject.Find("RightShoulderUI").transform.Find("Slider").Find("Handle Slide Area").Find("Handle").GetComponentInChildren<TextMeshProUGUI>(), GameObject.Find("RightShoulderUI").transform.parent.GetComponentInChildren<ActionMask>()));
+        weaponButtons.Add(new WeaponButton(GameObject.Find("LeftShoulderUI").transform.Find("Weapon").GetComponent<TextMeshProUGUI>(), GameObject.Find("LeftShoulderUI").GetComponentInChildren<Slider>(), GameObject.Find("LeftShoulderUI").transform.Find("Slider").Find("Handle Slide Area").Find("Handle").GetComponentInChildren<TextMeshProUGUI>(), GameObject.Find("LeftShoulderUI").transform.parent.GetComponentInChildren<ActionMask>()));
 
-        weaponName = rightWPNui.transform.parent.Find("Weapon").GetComponent<TextMeshProUGUI>();
-
-        modeText = GameObject.Find("RightArmMode").GetComponent<TextMeshProUGUI>();
-
-        rightWPNui.onValueChanged.AddListener(delegate { ChangeWPNmode(); });
+        foreach (WeaponButton weaponButton in weaponButtons)
+        {
+            weaponButton.slider.onValueChanged.AddListener(delegate { ChangeWPNmode(); });
+        }
 
         UpdateUI();
     }
 
     public void UpdateUI()
     {
+        foreach (WPNManager weapon in we)
+
         rightWPN = GameObject.Find("Player").transform.Find("Body").Find("Torso").Find("RightArm")
             .Find("RightArmWPN").GetComponentInChildren<WPNManager>();        
 
